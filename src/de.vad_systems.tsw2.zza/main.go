b6a72f0ca0dd3f4004c2aae092807fb7885fa5bd
FJones <service@vad-systems.de>
2022-04-17 15:53:52 +0200

    Add multichar handling

--- src/de.vad_systems.tsw2.zza/main.go
+++ src/de.vad_systems.tsw2.zza/main.go
@@ -19,6 +19,7 @@ func panicOnErr(err error) {
 }
 
 var charmapFileLoc *string
+
 func main() {
 	mode := flag.String("mode", "extract", "(extract|stitch|inplace) Mode to run in")
 	charmapFileLoc = flag.String("font-json", "BitmapTextFont.json", "Location of the BitmapTextFont.json to use")
@@ -48,7 +49,7 @@ func extract() {
 		font := charmap[i].ExportValue
 		dir := "./font" + strconv.Itoa(i)
 
-		if _, err := os.Open(dir) ; os.IsNotExist(err) {
+		if _, err := os.Open(dir); os.IsNotExist(err) {
 			err := os.Mkdir(dir, 0755)
 			panicOnErr(err)
 		}
@@ -59,15 +60,37 @@ func extract() {
 			img := image.NewGray(image.Rect(0, 0, int(char.SizeX), int(char.SizeY)))
 			for x := 0; x < img.Stride; x++ {
 				for y := 0; y < img.Rect.Dy(); y++ {
-					if val := uint8(font.RawTextureData[int(char.RawDataIndex) + y*img.Stride + x]); val > 127 {
-						img.Set(x, y, color.Gray{ Y: 255 })
+					if val := uint8(font.RawTextureData[int(char.RawDataIndex)+y*img.Stride+x]); val > 127 {
+						img.Set(x, y, color.Gray{Y: 255})
 					} else {
-						img.Set(x, y, color.Gray{ Y: 0 })
+						img.Set(x, y, color.Gray{Y: 0})
 					}
 				}
 			}
 
 			f, err := os.Create(dir + "/single_" + strconv.Itoa(c) + ".png")
+			panicOnErr(err)
+			err = png.Encode(f, img)
+			panicOnErr(err)
+			err = f.Close()
+			panicOnErr(err)
+		}
+
+		for c := range font.MultiChars {
+			char := font.MultiChars[c]
+
+			img := image.NewGray(image.Rect(0, 0, int(char.SizeX), int(char.SizeY)))
+			for x := 0; x < img.Stride; x++ {
+				for y := 0; y < img.Rect.Dy(); y++ {
+					if val := uint8(font.RawTextureData[int(char.RawDataIndex)+y*img.Stride+x]); val > 127 {
+						img.Set(x, y, color.Gray{Y: 255})
+					} else {
+						img.Set(x, y, color.Gray{Y: 0})
+					}
+				}
+			}
+
+			f, err := os.Create(dir + "/multi_" + strconv.Itoa(c) + ".png")
 			panicOnErr(err)
 			err = png.Encode(f, img)
 			panicOnErr(err)
@@ -108,9 +131,9 @@ func stitch() {
 			char.RawDataIndex = uint(len(charsData))
 			char.SizeX = uint8(img.Bounds().Dx())
 			char.SizeY = uint8(img.Bounds().Dy())
-			for y := 0 ; y < int(char.SizeY) ; y++ {
-				for x := 0 ; x < int(char.SizeX) ; x++ {
-					if r, _, _, _ := img.At(x, y).RGBA() ; r > 0 {
+			for y := 0; y < int(char.SizeY); y++ {
+				for x := 0; x < int(char.SizeX); x++ {
+					if r, _, _, _ := img.At(x, y).RGBA(); r > 0 {
 						charsData = append(charsData, 255)
 					} else {
 						charsData = append(charsData, 0)
@@ -121,15 +144,21 @@ func stitch() {
 
 		for c := range font.MultiChars {
 			char := &font.MultiChars[c]
-
-			multiCharData := make([]int, 0)
-			for b := int(char.RawDataIndex) ; b < int(char.RawDataIndex) + int(char.SizeX*char.SizeY) ; b++ {
-				multiCharData = append(multiCharData, font.RawTextureData[b])
-			}
-
+			f, err := os.Open(dir + "/multi_" + strconv.Itoa(c) + ".png")
+			panicOnErr(err)
+			img, err := png.Decode(f)
+			panicOnErr(err)
 			char.RawDataIndex = uint(len(charsData))
-			for m := range multiCharData {
-				charsData = append(charsData, multiCharData[m])
+			char.SizeX = uint8(img.Bounds().Dx())
+			char.SizeY = uint8(img.Bounds().Dy())
+			for y := 0; y < int(char.SizeY); y++ {
+				for x := 0; x < int(char.SizeX); x++ {
+					if r, _, _, _ := img.At(x, y).RGBA(); r > 0 {
+						charsData = append(charsData, 255)
+					} else {
+						charsData = append(charsData, 0)
+					}
+				}
 			}
 		}
 
@@ -140,7 +169,7 @@ func stitch() {
 			rawData = append(rawData, uint8(font.RawTextureData[b]))
 		}
 
-		err := os.WriteFile("BitmapTextFont." + strconv.Itoa(i) + ".texture", rawData, 0644)
+		err := os.WriteFile("BitmapTextFont."+strconv.Itoa(i)+".texture", rawData, 0644)
 		panicOnErr(err)
 	}
 
@@ -177,18 +206,40 @@ func inplace() {
 			img, err := png.Decode(f)
 			panicOnErr(err)
 			charData := make([]int, 0)
-			for y := 0 ; y < int(char.SizeY) ; y++ {
-				for x := 0 ; x < int(char.SizeX) ; x++ {
+			for y := 0; y < int(char.SizeY); y++ {
+				for x := 0; x < int(char.SizeX); x++ {
 					r, _, _, a := img.At(x, y).RGBA()
-					charData = append(charData, int((r/a) * 255))
+					charData = append(charData, int((r/a)*255))
 				}
 			}
-			for len(font.RawTextureData) < int(char.RawDataIndex) + len(charData) {
+			for len(font.RawTextureData) < int(char.RawDataIndex)+len(charData) {
 				font.RawTextureData = append(font.RawTextureData, 0)
 			}
 
 			for b := range charData {
-				font.RawTextureData[int(char.RawDataIndex) + b] = charData[b]
+				font.RawTextureData[int(char.RawDataIndex)+b] = charData[b]
+			}
+		}
+
+		for c := range font.MultiChars {
+			char := &font.MultiChars[c]
+			f, err := os.Open(dir + "/multi_" + strconv.Itoa(c) + ".png")
+			panicOnErr(err)
+			img, err := png.Decode(f)
+			panicOnErr(err)
+			charData := make([]int, 0)
+			for y := 0; y < int(char.SizeY); y++ {
+				for x := 0; x < int(char.SizeX); x++ {
+					r, _, _, a := img.At(x, y).RGBA()
+					charData = append(charData, int((r/a)*255))
+				}
+			}
+			for len(font.RawTextureData) < int(char.RawDataIndex)+len(charData) {
+				font.RawTextureData = append(font.RawTextureData, 0)
+			}
+
+			for b := range charData {
+				font.RawTextureData[int(char.RawDataIndex)+b] = charData[b]
 			}
 		}
 
@@ -197,7 +248,7 @@ func inplace() {
 			rawData = append(rawData, uint8(font.RawTextureData[b]))
 		}
 
-		err := os.WriteFile("BitmapTextFont." + strconv.Itoa(i) + ".texture", rawData, 0644)
+		err := os.WriteFile("BitmapTextFont."+strconv.Itoa(i)+".texture", rawData, 0644)
 		panicOnErr(err)
 	}
 
